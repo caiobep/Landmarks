bb40defe462eee554b34a3bd0724aea6ef033c6d
Caio Amaral Corrêa <caiobep@me.com>
2019-06-11 16:19:22 -0300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Caio Amaral Corrêa on 11/06/19.
+//  Copyright © 2019 Caio Amaral Corrêa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
